f73e1509dd59c36d48fc40e12c41acccf6c3bd52
lhanquiez <lhanquiez@symbiofi.com>
2025-09-16 10:19:16 +0200

    exo-7 validé

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var strSplit = str.split("");
+    var reverseArray = strSplit.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray
 };
 
 // Do not edit below this line
