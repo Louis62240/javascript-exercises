5803fe5799876728f012fb5a027dc85d0baa50c3
lhanquiez <lhanquiez@symbiofi.com>
2025-09-16 10:26:52 +0200

    exo-8 validé

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+      const newArray = [];
+
+  array.forEach((nbr) => {
+    if (!args.includes(nbr)) {
+      newArray.push(nbr);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
