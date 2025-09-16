a859a680f27198d2b1bc3902d9465def968ec042
lhanquiez <lhanquiez@symbiofi.com>
2025-09-16 10:15:42 +0200

    exo-6 validé

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, number) {
+  var textReturn = "";
+  if(Math.sign(number)===-1)
+  {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    textReturn = textReturn + text;
+  }
+  return textReturn;
 };
 
 // Do not edit below this line
